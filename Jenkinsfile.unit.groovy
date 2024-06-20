fc97221328c88877e5a59b3fa13bc3f63629e274
Enrique Bazúa <enriquegbazua@gmail.com>
2024-06-19 22:39:23 -0700

    modifying jenkinsfile

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -12,13 +12,36 @@ pipeline {
         stage('Unit tests') {
             steps {
                 sh 'make test-unit'
-                archiveArtifacts artifacts: 'results/*.xml'
+                archiveArtifacts artifacts: 'results/unit/*.xml'
+            }
+        }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/api/*.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/e2e/*.xml'
             }
         }
     }
     post {
         always {
-            junit 'results/*_result.xml'
+            junit 'results/**/*.xml'
+            cleanWs()
+        }
+        failure {
+            script {
+                def jobName = env.JOB_NAME
+                def buildNumber = env.BUILD_NUMBER
+                echo "Sending failure notification for Job: ${jobName}, Build: ${buildNumber}"
+                // mail to: 'example@example.com',
+                //      subject: "Job '${jobName}' (${buildNumber}) failed",
+                //      body: "Job '${jobName}' (${buildNumber}) failed. Please check Jenkins for more details."
+            }
         }
     }
 }
